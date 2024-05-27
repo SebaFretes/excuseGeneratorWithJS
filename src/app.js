79cfa8dcaa63a7b5399900fb677735b168d1b058
Seba Fretes <84410616+SebaFretes@users.noreply.github.com>
2024-05-27 21:58:19 +0000

    excuse generator created

--- src/app.js
+++ src/app.js
@@ -2,10 +2,27 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My sister", "The chef", "My fox"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car", "the laptop"];
+  let when = [
+    "before the meeting",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch"
+  ];
+
+  const aRandomItem = item => {
+    return item[Math.floor(Math.random() * item.length)];
+  };
+
+  let excuse = `${aRandomItem(who)} ${aRandomItem(action)} ${aRandomItem(
+    what
+  )} ${aRandomItem(when)}`;
+  document.getElementById("excuse").innerHTML = excuse;
 };
